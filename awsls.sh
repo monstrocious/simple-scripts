019df321339c17f106496a1e5c57a0bb23f465e9
monstrocious <moibrahim55@gmail.com>
2017-09-19 15:39:20 -0400

    Create awsls.sh

---
+++ awsls.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#Prints All EC2 instances in your region
+
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,State.Name,InstanceType,PrivateIpAddress,PublicIpAddress,Tags[?Key==`Name`].Value[]]' --output json | tr -d '\n[] "' | perl -pe 's/i-/\ni-/g' | tr ',' '\t' | sed -e 's/null/None/g' | grep '^i-' | column -t
